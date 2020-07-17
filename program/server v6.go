7d512c7f7de50a18b59d1db3cbe15e4fbb40a079
yyasha <50919655+yyasha@users.noreply.github.com>
2020-07-17 20:46:20 +0400

    Add files via upload

---
+++ program/server v6.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"encoding/json"
+	"github.com/yyasha/leap"
+	"net/http"
+	"runtime"
+)
+
+var Result []uint8
+
+func main() {
+	numcpu := runtime.NumCPU()
+	runtime.GOMAXPROCS(numcpu)
+
+	c, err := leap.Connect("localhost")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	go http.HandleFunc("/api", jsonServer)
+	go http.ListenAndServe(":80", nil)
+
+	for {
+		f, err := c.Frame()
+		if err != nil {
+			log.Fatal(err)
+		}
+		Result, _ = json.Marshal(f)
+		fmt.Println(string(Result))
+	}
+}
+
+func jsonServer(w http.ResponseWriter, r *http.Request) {
+
+    switch r.Method {
+    case "GET":
+            // j, _ := json.Marshal(frame)
+            w.Write(Result)
+    default:
+            w.WriteHeader(http.StatusMethodNotAllowed)
+            fmt.Fprintf(w, "I can't do that.")
+    }
+}
